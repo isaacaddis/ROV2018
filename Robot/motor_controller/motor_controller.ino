90755efd79fbf96f8d5f9ea21cad2223867419c7
Isaac Addis <isaacaddis21@gmail.com>
2017-10-13 11:37:09 -0700

    Update Motor Controller Code

--- Robot/motor_controller/motor_controller.ino
+++ Robot/motor_controller/motor_controller.ino
@@ -1,68 +1,43 @@
 /**
  * @author tedfoodlin
+ * @author jiajerho
+ * @author neil
  * 
- * ROV motor controller v2.2
+ * ROV relay motor controller v2.2
  */
  
-// Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.
 // Motors
-int dir1PinA = 2; // vertical motors
-int dir2PinA = 3; // vertical motors
-int dir1PinB = 4; // right motor
-int dir2PinB = 5; // right motor
-int dir1PinC = 6; // left motor
-int dir2PinC = 7; // left motor
+#define M1 1 //MOTOR 1
+#define MM1 2 //MOTOR 1
+#define M2 3 //MOTOR 2
+#define MM2 4 //MOTOR 2
+#define M3 5 //MOTOR 3
+#define MM3 6 //MOTOR 3
+#define M4 7 //MOTOR 4
+#define MM4 8 //MOTOR 4
 
 // Speed pins
-int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed
-
-const int brochePWM = 3; // Pwn pin
-const int in1 =  4; // Pin to activate bridge
-const int in2 =  5;
+int speed0 = 170; 
+int speed3 = 170;
+int speed1 = 170; 
+int speed2 = 170; 
 const int knob = 0; // Pin for Speed
+const int knob3 = 3; // Pin for Speed
 const int knob1 = 1; // Pin for Speed
 const int knob2 = 2; // Pin for Speed
 
-// Speed setpoints
-int speed0 = 170; 
-int speed1 = 170; 
-int speed2 = 170; 
-
-// Inversion (set to default at beginning)
-char inversion = '1';
-bool inverted = false;
-
 void setup() {
-  // initialize serial communication
+  // set up serial
   Serial.begin(9600);
 
-// Define L298N Dual H-Bridge Motor Controller Pins
-
-  pinMode(dir1PinA,OUTPUT);
-  pinMode(dir2PinA,OUTPUT);
-  pinMode(dir1PinB,OUTPUT);
-  pinMode(dir2PinB,OUTPUT);
-  pinMode(dir1PinC,OUTPUT);
-  pinMode(dir2PinC,OUTPUT);
-  pinMode(speedPinA,OUTPUT);
-  
-  // Output pin configuration 3
-  pinMode (brochePWM, OUTPUT);
-  pinMode (in1, OUTPUT);
-  pinMode (in2, OUTPUT);;
-  
-  // Inhibits the motor
-  analogWrite (brochePWM, 0);
-  digitalWrite (in1, LOW);
-  digitalWrite (in2, LOW);
-
-  delay(1500);
-  Serial.println(" ");
-  Serial.println("Motors enabled");
-  Serial.println(" ");
-  
-  // ask for inversion
-  Serial.println("Enter 1 or 2 to invert right and left turning.");
+  pinMode(M1, OUTPUT); //MOTOR 1 OUTPUT
+  pinMode(MM1, OUTPUT); // MOTOR 1 OUTPUT
+  pinMode(M2, OUTPUT); //MOTOR 2 OUTPUT
+  pinMode(MM2, OUTPUT); // MOTOR 2 OUTPUT
+  pinMode(M3, OUTPUT); //MOTOR 3 OUTPUT
+  pinMode(MM3, OUTPUT); // MOTOR 3 OUTPUT
+  pinMode(M4, OUTPUT); //MOTOR 4 OUTPUT
+  pinMode(MM4, OUTPUT); // MOTOR 4 OUTPUT
 }
 
 /**
@@ -74,59 +49,33 @@ void loop()
 {
   // Joystick power
   speed0 = analogRead(knob)/2; //vertical y-axis
-  speed1 = analogRead(knob1)/2; //left and right y-axis
-  speed2 = analogRead(knob2)/2; //left and right x-axis
-
-  // check for inversion
-  if (Serial.available()){
-    inversion = Serial.read();
-    if (inversion == '2'){
-      Serial.println("INVERSION MODE 2");
-      inverted = true;
-    } else {
-      Serial.println("INVERSION MODE 1");
-      inverted = false;
-    }
-  } else {
-    Serial.println("INVERSION MODE 1");
-    inverted = false;
-  }
-   
-/**
- * right and left motors
- */
+  speed3 = analogRead(knob3)/2; // vertical x-axis
+  speed1 = analogRead(knob1)/2; //lateral y-axis
+  speed2 = analogRead(knob2)/2; //lateral x-axis
+  
+  /**
+   * right and left motors
+   */
   // if y-axis power is negative
   if (speed1 < 50)
   {
-    allReverse();
+    allForwards();
   }
   // if y-axis power is positive
-  else if (speed1 > 400)
+  else if (speed1 > 500)
   {
-    allForwards();
+    allReverse();
   } 
   // if y-axis power is at resting state (for the sake of brevity we're calling it zero from now on)
   else 
   {
     // if x-axis power is negative (left direction)
     if (speed2 < 50){
-      if (inverted == false){
-        turnLeft();
-        Serial.println("Turning left");
-      } else if (inverted == true) {
-        turnRight();
-        Serial.println("Turning left");
-      }
+      turnLeft();
     } 
     // if x-axis power is positive (right direction)
     else if (speed2 > 400){
-      if (inverted == false){
-        turnRight();
-        Serial.println("Turning right");
-      } else if (inverted == true) {
-        turnLeft();
-        Serial.println("Turning right");
-      }
+      turnRight();
     } 
     // if x-axis power is zero 
     else {
@@ -150,83 +99,76 @@ void loop()
   // if y-axis power is zero
   else 
   {
-    verticalZero();
+    if (speed3 < 50){
+      pitchFwd();
+    }
+    else if (speed3 > 400){
+      pitchBack();
+    }
+    else {
+      verticalZero();
+    }
   }
 }
 
 // vertical motor functions and motion 
 void goUp(){
-  analogWrite(speedPinA, 255);
-  digitalWrite(dir1PinA, HIGH);
-  digitalWrite(dir2PinA, LOW);  
-  Serial.println("Going up");
+  pinMode(MM3, HIGH); //MOTOR 3 CLOCKWISE
+  pinMode(MM4, HIGH); // MOTOR 4 CLOCKWISE
+//  Serial.println("Go Up");
 }
 void goDown(){
-  analogWrite(speedPinA, 255);
-  digitalWrite(dir1PinA, LOW);
-  digitalWrite(dir2PinA, HIGH); 
-  Serial.println("Going down"); 
+  pinMode(M3, HIGH); //MOTOR 3 COUNTER CLOCKWISE
+  pinMode(M4, HIGH); // MOTOR 4 COUNTER CLOCKWISE
+//  Serial.println("Go Down");
+}
+void pitchFwd(){
+  pinMode(M3, HIGH); 
+  pinMode(MM4, HIGH);
+//  Serial.println("Pitch fowards");
+}
+void pitchBack(){
+  pinMode(MM3, HIGH);
+  pinMode(M3, HIGH);
+//  Serial.println("Pitch backwards");
 }
 void verticalZero(){
-  analogWrite(speedPinA, 0);
-  digitalWrite(dir1PinA, LOW);
-  digitalWrite(dir2PinA, LOW);
-  Serial.println("No vertical movement");
-}
-
-// lateral motor functions
-void rightReverse(){
-  analogWrite(speedPinA, 255); 
-  digitalWrite(dir1PinB, HIGH);
-  digitalWrite(dir2PinB, LOW);
-}
-void rightForwards(){
-  analogWrite(speedPinA, 255);
-  digitalWrite(dir1PinB, LOW);
-  digitalWrite(dir2PinB, HIGH);
-}
-void rightZero(){
-  analogWrite(speedPinA, 255);
-  digitalWrite(dir1PinB, LOW);
-  digitalWrite(dir2PinB, LOW);
-}
-void leftReverse(){
-  analogWrite(speedPinA, 255); 
-  digitalWrite(dir1PinC, HIGH);
-  digitalWrite(dir2PinC, LOW);  
-}
-void leftForwards(){
-  analogWrite(speedPinA, 255);
-  digitalWrite(dir1PinC, LOW);
-  digitalWrite(dir2PinC, HIGH);  
-}
-void leftZero(){
-  analogWrite(speedPinA, 0);
-  digitalWrite(dir1PinC, LOW);
-  digitalWrite(dir2PinC, LOW);  
+  pinMode(M3, LOW); //MOTOR 3 STOP
+  pinMode(MM3, LOW); // MOTOR 3 STOP
+  pinMode(M4, LOW); //MOTOR 4 STOP
+  pinMode(MM4, LOW); // MOTOR 4 STOP
+//  Serial.println("Vertical zero");
 }
 
 // lateral motion functions
 void allForwards(){
-    rightReverse();
-  leftReverse();
-  Serial.println("Forwards");
+  pinMode(M1, HIGH); //MOTOR 1 CLOCKWISE
+  pinMode(M2, HIGH); //MOTOR 2 CLOCKWISE
+//  Serial.println("All forwards");
 }
 void allReverse(){
-  rightForwards();
-  leftForwards();
-  Serial.println("Reverse");
+  pinMode(MM1, HIGH); //MOTOR 1 COUNTER CLOCKWISE
+  pinMode(MM2, HIGH); //MOTOR 2 COUNTER CLOCKWISE
+//  Serial.println("All reverse");
 }
 void allZero(){
-  rightZero();
-  leftZero();
-  Serial.println("No lateral movement");
+  pinMode(M1, LOW); //MOTOR 1 STOP
+  pinMode(MM1, LOW); // MOTOR 1 STOP
+  pinMode(M2, LOW); //MOTOR 2 STOP
+  pinMode(MM2, LOW); // MOTOR 2 STOP
+  pinMode(M3, LOW); //MOTOR 3 STOP
+  pinMode(MM3, LOW); // MOTOR 3 STOP
+  pinMode(M4, LOW); //MOTOR 4 STOP
+  pinMode(MM4, LOW); // MOTOR 4 STOP
+//  Serial.println("All zero");
 }
 void turnLeft(){
-  rightForwards();
-  leftReverse();
+  pinMode(M1, HIGH); //MOTOR 1 CLOCKWISE
+  pinMode(MM2, HIGH); //MOTOR 2 COUNTER CLOCKWISE
+//  Serial.println("Turn left");
 }
 void turnRight(){
-  leftForwards();
-  rightReverse();
+  pinMode(MM1, HIGH); //MOTOR 1 COUNTER CLOCKWISE
+  pinMode(M2, HIGH); //MOTOR 2 CLOCKWISE
+//  Serial.println("Turn right");
 }
