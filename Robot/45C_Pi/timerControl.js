cc657173df112f6ffd2c0976bd5469dd56cfb4f7
Isaac Addis <isaacaddis21@gmail.com>
2017-10-18 09:12:45 -0700

    Add toggle and display in minute/seconds for timer

--- Robot/45C_Pi/timerControl.js
+++ Robot/45C_Pi/timerControl.js
@@ -2,41 +2,44 @@
 	Underlying logic and control for the timer used on the 2018 Graphical User Interface (GUI) for 45C Robotics
 	@author: Isaac Addis (github.com/isaacaddis)
 */
-(function($) {
-	toggleOn = false;
-	function toggle(){
-		if(toggleOn){
-			document.getElementById("#play").className = "pause fa fa-pause";
-		}
-		else{
-			document.getElementById("#play").className = "play fa fa-play";
-		}
+function toggle(state){
+	if(state){
+		document.getElementById("#play").className = "pause fa fa-pause";
 	}
+	else if(!state){
+		document.getElementById("#play").className = "play fa fa-play";
+	}
+}
+(function($) {
+
     $(function() {
         var $play  = $('.play'),
             $pause = $('.pause'),
-            $stop  = $('.stop'),
+//             $stop  = $('.stop'),
             $time  = $('.time-input'),
             $timer = $('.timer');
 
         var timer = new Timer({
             onstart : function(millisec) {
-                var sec = Math.round(millisec / 1000);
-                $timer.text(sec);
-				toggleOn = true;
+		//there are 1000*60 = 60,000 milliseconds in one minute
+                var min = Math.round(millisec/60000);
+                $timer.text(min);
+		toggle(true);
             },
             ontick  : function(millisec) {
                 var sec = Math.round(millisec / 1000);
-                $timer.text(sec);
+		var minutes = Math.floor(millisec / 60000);
+		var seconds = ((millisec % 60000) / 1000).toFixed(0);
+		$timer.text(minutes + ":" + (seconds < 10 ? '0' : '') + seconds);
             },
             onpause : function() {
                 $timer.text('pause');
-				toggleOn = false;
+		toggle(false);
 
             },
-            onstop  : function() {
-                $timer.text('stop');
-            },
+//             onstop  : function() {
+//                 $timer.text('stop');
+//             },
             onend   : function() {
                 $timer.text('end');
             }
