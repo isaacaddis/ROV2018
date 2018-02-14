c54014eb471b73af7ab493e95e7c319d30fa96de
Isaac Addis <isaacaddis21@gmail.com>
2018-02-14 09:34:56 -0800

    Update script.js

--- Robot/45C_Pi/static/js/script.js
+++ Robot/45C_Pi/static/js/script.js
@@ -7,18 +7,18 @@ function process(msg){
 	msg = String(msg);
 	if(msg.includes("T1"){
 		msg = msg.substring(2,msg.length-1);
-		$('#temp).text(msg);
+		$('#temp').text(msg);
 	}
 	else if(msg.includes("T2"){
 		msg = msg.substring(2,msg.length-1);
-		$('#temp2).text(msg);
+		$('#temp2').text(msg);
 	}
 	else if(msg.includes("V1"){
 		msg = msg.substring(2,msg.length-1);
-		$('#volt).text(msg);
+		$('#volt').text(msg);
 	}
 	else if(msg.includes("V2"){
 		msg = msg.substring(2,msg.length-1);
-		$('#volt2).text(msg);
+		$('#volt2').text(msg);
 	}
 }
