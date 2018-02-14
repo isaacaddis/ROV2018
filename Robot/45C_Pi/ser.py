c91be18cf69bf86f4b8a23fc8d80fb0a4c6737bd
Isaac Addis <isaacaddis21@gmail.com>
2018-02-14 09:04:03 -0800

    Update ser.py

--- Robot/45C_Pi/ser.py
+++ Robot/45C_Pi/ser.py
@@ -1,18 +1,22 @@
 '''
 	@author isaacaddis
 
-	file to handle getting serial input from the onboard Arduino.
 '''
 import serial
-# import threading
+from driver import app
 
+#Start serial coms
 ser = serial.Serial('/dev/ttyAMA0',9600,timeout=1)
 ser.isOpen()
-message = ""
-# while True:
-def ser():
-        try:
-			message = ser.readline()
-			return message
-		catch:
-			print("Did not read a message")      
+
+'''
+	Loop for capturing serial output of Arduino and passing it to the GUI
+'''	
+while True:
+	try:
+		message = ser.readline()			
+		app.evaluate_javascript("process("+String(message)+")")
+	catch:
+		print("There was an error!")
+	except KeyboardInterrupt:
+		ser.close()
