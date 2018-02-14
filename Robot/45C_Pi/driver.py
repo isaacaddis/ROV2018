4dc351ede215e861e986db645ff14f3969cbb1f8
Isaac Addis <isaacaddis21@gmail.com>
2018-02-14 08:55:04 -0800

    Update driver.py

--- Robot/45C_Pi/driver.py
+++ Robot/45C_Pi/driver.py
@@ -6,19 +6,13 @@ import time
 
 from PyQt4 import QtGui
 
-ser = serial.Serial('/dev/ttyAMA0',9600,timeout=1)
-ser.isOpen()
-message = ""
+# ser = serial.Serial('/dev/ttyAMA0',9600,timeout=1)
+# ser.isOpen()
+# message = ""
 
-# Initial confiurations
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-# GUI initializations
 app = htmlPy.AppGUI(title=u"45C Robotics 2018 GUI", maximized=True, plugins=True)
 
-
-# GUI configurations
 app.static_path = os.path.join(BASE_DIR, "static/")
 app.template_path = os.path.join(BASE_DIR, "templates/")
 
@@ -33,10 +27,3 @@ app.template = ("index.html", {})
 
 if __name__ == "__main__":
     app.start()
-	while True:
-		try:
-			message = ser.readline()
-			app.evaluate_javascript("process("+message+")")
-			time.sleep(0.5)
-		except KeyboardInterrupt:
-			ser.close()
