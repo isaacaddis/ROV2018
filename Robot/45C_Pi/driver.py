3a5dc13a73a9f64618ee99212b5928ca6bafcde1
Isaac Addis <isaacaddis21@gmail.com>
2018-02-14 09:06:25 -0800

    Update driver.py

--- Robot/45C_Pi/driver.py
+++ Robot/45C_Pi/driver.py
@@ -6,12 +6,8 @@ import time
 
 from PyQt4 import QtGui
 
-# ser = serial.Serial('/dev/ttyAMA0',9600,timeout=1)
-# ser.isOpen()
-# message = ""
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app = htmlPy.AppGUI(title=u"45C Robotics 2018 GUI", maximized=True, plugins=True)
+app = htmlPy.AppGUI(title=u"45C Robotics 2018", maximized=True, plugins=True)
 
 app.static_path = os.path.join(BASE_DIR, "static/")
 app.template_path = os.path.join(BASE_DIR, "templates/")
