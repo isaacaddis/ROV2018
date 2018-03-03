cffa530d6e0a2e2574e42eca894f1de679125854
isaacaddis <isaacaddis>
2018-03-03 14:26:54 -0800

    Add GUI to main vision code

--- Robot/vision/vision.py
+++ Robot/vision/vision.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import cv2
 import numpy as np
 import math
@@ -9,6 +10,7 @@ import frontCam
 import backCam
 import sys
 from PyQt4 import QtGui
+from PyQt4 import QtCore
 
 __author__ = "isaacaddis"
 
@@ -23,10 +25,11 @@ state = 1
 '''
 	Main Loop
 '''
-class Main(QtGui.QWidget):
+class MainApp(QtGui.QWidget):
     def __init__(self):
-        super(Main,self).__init__()
+        super(MainApp,self).__init__()
         self.initUI()
+        self.initCam()
     def initUI(self):
         exitButton = Qt.Gui.QPushButton("OK")
         hbox = QtGui.QHboxLayout()
@@ -39,10 +42,28 @@ class Main(QtGui.QWidget):
         self.showFullScreen()
         self.setWindowTitle('45c')
         self.show()
+    def initCam(self):
+		self.capture = cv2.VideoCapture(0)
+		self.capture.set(cv2.CV_CAP_FRAME_WIDTH,self.video_size.width())
+		self.capture.set(cv2.CV_CAP_PROP_FRAME_HEIGHT,self.video_size.height())
+		self.timer=QTimer()
+		self.timer.timeout.connect(self.display_video_stream)
+		self.timer.start(30)
+    def vis(self):
+        _,frame = self.capture.read()
+        frame = cv2.cvtColor(frame, cv2.cv.CV_BGR2RGB)
+        frame = cv2.flip(frame,1)
+        image = QImage(frame,frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_RGB888)
+        self.image_label.setPixmap(QPixmap.fromImage(image)
+
 def main():
-    app = QtGui.QApplication(sys.argv)
-    main = Main()
-    sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    win = MainApp()
+    win.show()
+    sys.exit(app.exec_())	
+if __name__ == "__main__":
+	main()
+	
 '''
 while True:
 	#Default (State 1) - Front Camera
