9e3d82abffd3f85e1591a6d79705c3a2bf8fa22c
isaacaddis <isaacaddis21@gmail.com>
2018-03-03 15:15:32 -0800

    Incorporate CV script into GUI

--- Robot/vision/vision.py
+++ Robot/vision/vision.py
@@ -55,10 +55,32 @@ class MainApp(QtGui.QWidget):
         #30 fps
     	self.timer.start(30)
     def vis(self):
-        _,frame = self.capture.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        frame = cv2.flip(frame,1)
-        image = QImage(frame,frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_RGB888)
+	ret, frame = self.capture.read()
+	kernel = np.ones((5,5), np.uint8)
+	img = cv2.Canny(frame, 50, 100)
+	img = cv2.dilate(img, kernel, iterations=1)	
+	img = cv2.erode(img, kernel, iterations=1)
+	_, cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	for c in cnts:
+		# If contours are too small or large, ignore them:
+		if cv2.contourArea(c)<100:
+			continue
+		elif cv2.contourArea(c)>2000:
+			continue
+        cv2.drawContours(frame, [c], -1, (0,255,0), 3)
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        # Find center point of contours:
+        M = cv2.moments(c)
+        cX = int(M['m10'] /M['m00'])
+        cY = int(M['m01'] /M['m00'])
+        centers.append([cX,cY])		
+        if len(centers) >=2:
+			dx= centers[0][0] - centers[1][0]
+			dy = centers[0][1] - centers[1][1]
+			D = np.sqrt(dx*dx+dy*dy)
+			print(D)
+        image = QImage(img,img.shape[1], img.shape[0], img.strides[0], QImage.Format_RGB888)
         self.image_label.setPixmap(QPixmap.fromImage(image))
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
