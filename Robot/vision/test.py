2ef1dea2287fc6c091668749f2c73446e8bad223
isaacaddis <isaacaddis21@gmail.com>
2018-03-26 14:42:11 -0700

    Distance measure

--- Robot/vision/test.py
+++ Robot/vision/test.py
@@ -28,41 +28,43 @@ def vis(mirror=True):
 			img = cv2.Canny(img,100,200)
 			img = cv2.bilateralFilter(img, 11, 17, 17)
 			_,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+			centers = []
 			if len(cnts)>0:
 				cntsSorted = sorted(cnts,key=cv2.contourArea,reverse=True)
-				areas = np.array([cv2.contourArea(cnt) for cnt in cnts])
 				cnts1=cntsSorted[0]
 				cnts2=cntsSorted[1]
+				cntsShort = [cnts1,cnts2]
 				#print(cntsSorted)
-				for c in cnts:
-					x,y,w,h = cv2.boundingRect(cnts1)
-					cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-					x,y,w,h = cv2.boundingRect(cnts2)
-					cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-					#cv2.drawContours(frame, [c], 0, (0,255,0), 3)
-					#For debug
-					#print(c)
-					#print(it)
-					# If contours are too small or large, ignore them:
-					if cv2.contourArea(c)<100:
-						print("Too small")	
-						continue
-					if cv2.contourArea(c)>2000:
-						print("Too big!")
-						continue
-					#x,y,w,h = cv2.boundingRect(cnts)
-					#cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-					# Find center point of contours:
-					M = cv2.moments(c)
-					centers = []
-					cX = int(M['m10'] /M['m00'])
-					cY = int(M['m01'] /M['m00'])
-					centers.append([cX,cY])		
-					if len(centers) >=2:
-						dx= centers[0][0] - centers[1][0]
-						dy = centers[0][1] - centers[1][1]
-						D = np.sqrt(dx*dx+dy*dy)
-						print(D)
+				x,y,w,h = cv2.boundingRect(cnts1)
+				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+				x,y,w,h = cv2.boundingRect(cnts2)
+				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+				#cv2.drawContours(frame, [c], 0, (0,255,0), 3)
+				#For debug
+				#print(c)
+				#print(it)
+				# If contours are too small or large, ignore them:
+				#if cv2.contourArea(c)<100:
+				#	print("Too small")	
+				#	continue
+				#if cv2.contourArea(c)>2000:
+				#	print("Too big!")
+				#	continue
+				#x,y,w,h = cv2.boundingRect(cnts)
+				#cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+				# Find center point of contours:
+				M = cv2.moments(cnts1)
+				cX = int(M['m10'] /M['m00'])
+				cY = int(M['m01'] /M['m00'])
+				centers.append([cX,cY])		
+				M = cv2.moments(cnts2)
+				cX = int(M['m10'] /M['m00'])
+				cY = int(M['m01'] /M['m00'])
+				centers.append([cX,cY])		 
+				dx= centers[0][0] - centers[1][0]
+				dy = centers[0][1] - centers[1][1]
+				D = np.sqrt(dx*dx+dy*dy)
+				print(D)
 			cv2.imshow("45c Robotics",frame)
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
