6b4d26aa737f998463b0d73478b18d22ad938284
isaacaddis <isaacaddis21@gmail.com>
2018-03-26 13:12:59 -0700

    Working test script - refine contour selection

--- Robot/vision/test.py
+++ Robot/vision/test.py
@@ -9,35 +9,37 @@ import sys
 #from PyQt4.QtGui import *
 import os
 
-__author__ = "isaacaddis"
-os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt'
+#__author__ = "isaacaddis"
+#os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt'
 
-def vis(mirror=False):
-	'''			
-		Vertical Distance mayn
-	'''
+def vis(mirror=True):
+	#it = 0
 	cap = cv2.VideoCapture(0)
-	while True:
-		ret_val, frame = cap.read()
-		if mirror: 
-			img = cv2.flip(frame, 1)
+	if mirror:
+		while cap.isOpened():
+			#it+=1
+			ret_val, frame = cap.read()
+			#img = cv2.flip(frame, 1)
 			kernel = np.ones((5,5), np.uint8)
-			img = cv2.erode(img, kernel, iterations=1)
+			img = cv2.erode(frame, kernel, iterations=1)
 			img = cv2.dilate(img, kernel, iterations=1)
 			img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
 			img = cv2.bilateralFilter(img, 11, 17, 17)
 			'''img	 = cv2.Canny(img, 30, 200) '''
-			_,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-			if len(cnts)==2:
+			_,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+			if len(cnts)>0:
 				for c in cnts:
 					cv2.drawContours(frame, [c], 0, (0,255,0), 3)
 					#For debug
 					print(c)
+					#print(it)
 					# If contours are too small or large, ignore them:
-					'''if cv2.contourArea(c)<100:
+					if cv2.contourArea(c)<100:
 						print("Too small")	
 						continue
-					'''
+					if cv2.contourArea(c)>2000:
+						print("Too big!")
+						continue
 					#x,y,w,h = cv2.boundingRect(cnts)
 					#cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
 					# Find center point of contours:
@@ -51,7 +53,8 @@ def vis(mirror=False):
 						dy = centers[0][1] - centers[1][1]
 						D = np.sqrt(dx*dx+dy*dy)
 						print(D)
-					if cv2.waitKey(1) & 0xFF == ord('q'):
-						break
-		cv2.imshow("45c Robotics",frame)	
+				cv2.imshow("45c Robotics",frame)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+				
 vis(mirror=True)
