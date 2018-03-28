5bf8ec0f75e23a2d0ccd38d2ab338b39955f1ebd
Jiajer Ho <jjh10422@gmail.com>
2018-03-28 14:14:15 -0700

    Update driver.py

--- Robot/45C_Pi/driver.py
+++ Robot/45C_Pi/driver.py
@@ -6,7 +6,7 @@ import time
 from PyQt4 import QtGui
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app = htmlPy.AppGUI(title=u"45C Robotics 2018",developer_mode=True, maximized=True)
+app = htmlPy.AppGUI(title=u"45C Robotics 2018", maximized=True)
 
 app.static_path = os.path.join(BASE_DIR, "static/")
 app.template_path = os.path.join(BASE_DIR, "templates/")
