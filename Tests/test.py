8a06e4c0bcf428faab255b22bca82ee434b7d657
isaacaddis <isaacaddis21@gmail.com>
2018-03-29 01:08:43 -0700

    Move displaying

--- Tests/test.py
+++ Tests/test.py
@@ -15,6 +15,8 @@ import os
 def vis(mirror=True):
 	#it = 0
 	cap = cv2.VideoCapture(1)
+	cv2.namedWindow("45c Robotics", cv2.WND_PROP_FULLSCREEN)
+	cv2.setWindowProperty("45c Robotics", cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)	
 	if mirror:
 		while cap.isOpened():
 			#it+=1
@@ -29,17 +31,15 @@ def vis(mirror=True):
 			img = cv2.bilateralFilter(img, 11, 17, 17)
 			_,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 			centers = []
-			if len(cnts)>0:
+			if len(cnts)>=2:
 				cntsSorted = sorted(cnts,key=cv2.contourArea,reverse=True)
 				cnts1=cntsSorted[0]
 				cnts2=cntsSorted[1]
 				cntsShort = [cnts1,cnts2]
-				#print(cntsSorted)
 				x,y,w,h = cv2.boundingRect(cnts1)
 				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
 				x,y,w,h = cv2.boundingRect(cnts2)
 				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-				# Find center point of contours:
 				M = cv2.moments(cnts1)
 				cX = int(M['m10'] /M['m00'])
 				cY = int(M['m01'] /M['m00'])
@@ -51,14 +51,11 @@ def vis(mirror=True):
 				dx= centers[0][0] - centers[1][0]
 				dy = centers[0][1] - centers[1][1]
 				D = abs(dy)
-				#print(D)
-				cv2.putText(frame,str(D),
+			#TODO: Calibrate
+			cv2.putText(frame,str(D),
 			(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-			2.0, (0, 255, 0), 3)
-				cv2.namedWindow("45c Robotics", cv2.WND_PROP_FULLSCREEN)
-				cv2.setWindowProperty("45c Robotics", cv2.WND_PROP_FULLSCREEN,
-								  cv2.WINDOW_FULLSCREEN)			
-				cv2.imshow("45c Robotics",frame)
-				if cv2.waitKey(1) & 0xFF == ord('q'):
+			2.0, (0, 255, 0), 3)		
+			cv2.imshow("45c Robotics",frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
 					break
 vis(mirror=True)
