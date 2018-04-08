d0019391684e989e7ccb495c445b45212bbd6400
isaacaddis <isaacaddis21@gmail.com>
2018-04-08 11:50:51 -0700

    Keyboard input

--- Robot/vision/vision.py
+++ Robot/vision/vision.py
@@ -2,6 +2,7 @@
 
 from PySide.QtCore import *
 from PySide.QtGui import *
+import keyboard
 import cv2
 import qdarkstyle
 import sys
@@ -85,79 +86,92 @@ class MainApp(QWidget):
             self.timer = QTimer()
             self.timer.timeout.connect(self.cap2)
             self.timer.start(30)
+    #True means turn vision on
+    capState = True
     def cap(self):
+        if keyboard.is_pressed('A'):
+            #Alternate between true and false
+            self.capState^=True
+            print("State of capState is now: ")
+            print(self.capState)
         ret_val, frame = self.capture.read()
-        # ret_val2, frame2 = self.capture2.read()
-        kernel = np.ones((5,5), np.uint8)
-        img = cv2.GaussianBlur(frame,(5,5),0)
-        img = cv2.erode(img, kernel, iterations=1)
-        img = cv2.dilate(img, kernel, iterations=1)
-        img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
-        img = cv2.Canny(img,100,200)
-        img = cv2.bilateralFilter(img, 11, 17, 17)
-        _,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        centers = []
-        if len(cnts)>=2:
-            cntsSorted = sorted(cnts,key=cv2.contourArea,reverse=True)
-            cnts1=cntsSorted[0]
-            cnts2=cntsSorted[1]
-            cntsShort = [cnts1,cnts2]
-            x,y,w,h = cv2.boundingRect(cnts1)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-            x,y,w,h = cv2.boundingRect(cnts2)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-            M = cv2.moments(cnts1)
-            cX = int(M['m10'] /M['m00'])
-            cY = int(M['m01'] /M['m00'])
-            centers.append([cX,cY])
-            M = cv2.moments(cnts2)
-            cX = int(M['m10'] /M['m00'])
-            cY = int(M['m01'] /M['m00'])
-            centers.append([cX,cY])
-            dx= centers[0][0] - centers[1][0]
-            dy = centers[0][1] - centers[1][1]
-            D = abs(dy)
-            #TODO: Calibrate
-            cv2.putText(frame,str(D),
-            (frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-            2.0, (0, 255, 0), 3)
+        if self.capState:
+            kernel = np.ones((5,5), np.uint8)
+            img = cv2.GaussianBlur(frame,(5,5),0)
+            img = cv2.erode(img, kernel, iterations=1)
+            img = cv2.dilate(img, kernel, iterations=1)
+            img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
+            img = cv2.Canny(img,100,200)
+            img = cv2.bilateralFilter(img, 11, 17, 17)
+            _,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            centers = []
+            if len(cnts)>=2:
+                cntsSorted = sorted(cnts,key=cv2.contourArea,reverse=True)
+                cnts1=cntsSorted[0]
+                cnts2=cntsSorted[1]
+                cntsShort = [cnts1,cnts2]
+                x,y,w,h = cv2.boundingRect(cnts1)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                x,y,w,h = cv2.boundingRect(cnts2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                M = cv2.moments(cnts1)
+                cX = int(M['m10'] /M['m00'])
+                cY = int(M['m01'] /M['m00'])
+                centers.append([cX,cY])
+                M = cv2.moments(cnts2)
+                cX = int(M['m10'] /M['m00'])
+                cY = int(M['m01'] /M['m00'])
+                centers.append([cX,cY])
+                dx= centers[0][0] - centers[1][0]
+                dy = centers[0][1] - centers[1][1]
+                D = abs(dy)
+                #TODO: Calibrate
+                cv2.putText(frame,str(D),
+                (frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+                2.0, (0, 255, 0), 3)
         image = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_RGB888)
         self.image_label.setPixmap(QPixmap.fromImage(image))
+    capState2 = True
     def cap2(self):
+        if keyboard.is_pressed('L'):
+            self.capState2^=True
+            print("State of capState2 is now: ")
+            print(self.capState2)
         ret_val, frame = self.capture2.read()
-        kernel = np.ones((5,5), np.uint8)
-        img = cv2.GaussianBlur(frame,(5,5),0)
-        img = cv2.erode(img, kernel, iterations=1)
-        img = cv2.dilate(img, kernel, iterations=1)
-        img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
-        img = cv2.Canny(img,100,200)
-        img = cv2.bilateralFilter(img, 11, 17, 17)
-        _,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        centers = []
-        if len(cnts)>=2:
-            cntsSorted = sorted(cnts,key=cv2.contourArea,reverse=True)
-            cnts1=cntsSorted[0]
-            cnts2=cntsSorted[1]
-            cntsShort = [cnts1,cnts2]
-            x,y,w,h = cv2.boundingRect(cnts1)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-            x,y,w,h = cv2.boundingRect(cnts2)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-            M = cv2.moments(cnts1)
-            cX = int(M['m10'] /M['m00'])
-            cY = int(M['m01'] /M['m00'])
-            centers.append([cX,cY])
-            M = cv2.moments(cnts2)
-            cX = int(M['m10'] /M['m00'])
-            cY = int(M['m01'] /M['m00'])
-            centers.append([cX,cY])
-            dx= centers[0][0] - centers[1][0]
-            dy = centers[0][1] - centers[1][1]
-            D = abs(dy)
-            #TODO: Calibrate
-            cv2.putText(frame,str(D),
-            (frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-            2.0, (0, 255, 0), 3)
+        if self.capState2:
+            kernel = np.ones((5,5), np.uint8)
+            img = cv2.GaussianBlur(frame,(5,5),0)
+            img = cv2.erode(img, kernel, iterations=1)
+            img = cv2.dilate(img, kernel, iterations=1)
+            img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
+            img = cv2.Canny(img,100,200)
+            img = cv2.bilateralFilter(img, 11, 17, 17)
+            _,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            centers = []
+            if len(cnts)>=2:
+                cntsSorted = sorted(cnts,key=cv2.contourArea,reverse=True)
+                cnts1=cntsSorted[0]
+                cnts2=cntsSorted[1]
+                cntsShort = [cnts1,cnts2]
+                x,y,w,h = cv2.boundingRect(cnts1)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                x,y,w,h = cv2.boundingRect(cnts2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                M = cv2.moments(cnts1)
+                cX = int(M['m10'] /M['m00'])
+                cY = int(M['m01'] /M['m00'])
+                centers.append([cX,cY])
+                M = cv2.moments(cnts2)
+                cX = int(M['m10'] /M['m00'])
+                cY = int(M['m01'] /M['m00'])
+                centers.append([cX,cY])
+                dx= centers[0][0] - centers[1][0]
+                dy = centers[0][1] - centers[1][1]
+                D = abs(dy)
+                #TODO: Calibrate
+                cv2.putText(frame,str(D),
+                (frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+                2.0, (0, 255, 0), 3)
         image2 = QImage(frame2, frame2.shape[1], frame2.shape[0], frame2.strides[0], QImage.Format_RGB888)
         self.image_label2.setPixmap(QPixmap.fromImage(image2))
 if __name__ == "__main__":
