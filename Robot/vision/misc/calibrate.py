daccbe2f2098e29fe88694a1e2528c5a1b57e750
isaacaddis <isaacaddis21@gmail.com>
2018-04-15 15:46:29 -0700

    Calibrate

--- Robot/vision/misc/calibrate.py
+++ Robot/vision/misc/calibrate.py
@@ -1,16 +1,15 @@
-'''	
+'''
 	Open window sized 600x600 pixels for calibration
 '''
 import cv2
 
 def show(mirror=True):
-	#it = 0
-	cap = cv2.VideoCapture(1)
-	cv2.namedWindow('45c Robotics',WINDOW_NORMAL)
-	cv2.resizeWindow('45c Robotics', 600,600)	
+	cap = cv2.VideoCapture(0)
+#	cv2.namedWindow('45c Robotics',cv2.WINDOW_NORMAL)
+#	cv2.resizeWindow('45c Robotics', 600,600)
 	if mirror:
 		while cap.isOpened():
-			#it+=1
+                        print("Opened")
 			ret_val, frame = cap.read()
 			cv2.imshow('45c Robotics',frame)
 show(mirror=True)
