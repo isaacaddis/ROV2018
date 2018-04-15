62c3191fa5841fd8bd66438a375e123e7d042945
isaacaddis <isaacaddis21@gmail.com>
2018-04-14 20:39:34 -0700

    Both cameras

--- Tests/test.py
+++ Tests/test.py
@@ -19,7 +19,8 @@ def vis(mirror=True):
      (_)  \______/ (_______/  |/   \__/(_______)|/ \___/ (_______)   )_(   \_______/(_______/\_______)
                                                                                                       
  """)
-    vis = True
+    vis1 = True
+    vis2 = True
     cap = cv2.VideoCapture(0)
     cap.set(3,320)
     cap.set(4,216)
@@ -29,17 +30,20 @@ def vis(mirror=True):
     cap2.set(4,216)
     cap2.set(5,15)
     cv2.namedWindow("45c Robotics", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("45c Robotics", 600,600)
+    cv2.resizeWindow("45c Robotics", 960,540)
     cv2.namedWindow("45c Robotics 2",cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("45c Robotics 2",600,600)
+    cv2.resizeWindow("45c Robotics 2",960,540)
     if mirror:
         while cap.isOpened():
             ret_val, frame = cap.read()
             ret_val2, frame2 = cap2.read()
-            if keyboard.is_pressed('v'):
-            	vis^=True
-            	print("Vision state switched.")
-            if vis == True:
+            if keyboard.is_pressed('a'):
+            	vis1^=True
+            	print("Vision state switched on Camera 0.")
+            if keyboard.is_pressed('l'):
+            	vis2^=True
+            	print("Vision state switched on Camera 1.")
+            if vis1 == True:
                 kernel = np.ones((5,5), np.uint8)
                 img = cv2.GaussianBlur(frame,(5,5),0)
                 img = cv2.erode(img, kernel, iterations=1)
@@ -72,7 +76,41 @@ def vis(mirror=True):
                         	print(D)
                          	#TODO: Calibrate
                          	cv2.putText(frame,str(D),(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
-            cv2.putText(frame,"Vision State: "+str(vis),(frame.shape[1] - 400, frame.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+                if vis2 == True:
+                    kernel = np.ones((5,5), np.uint8)
+                    img = cv2.GaussianBlur(frame2,(5,5),0)
+                    img = cv2.erode(img, kernel, iterations=1)
+                    img = cv2.dilate(img, kernel, iterations=1)
+                    img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
+                    img = cv2.Canny(img,100,200)
+                    img = cv2.bilateralFilter(img, 11, 17, 17)
+                    _,cnts, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    centers = []
+                    if len(cnts)>=2:
+                        for c in cnts:
+                            if cv2.contourArea(c)<100:
+                                continue
+                            if cv2.contourArea(c)>2000:
+                                continue
+                            x,y,w,h = cv2.boundingRect(c)
+                            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                            M = cv2.moments(c)
+                            if M['m00']!=0:
+                            	cX = int(M['m10'] /M['m00'])
+                            	cY = int(M['m01'] /M['m00'])
+                            	centers.append([cX,cY])
+                            else:
+                            	print("Impossible to append to centers.")
+                            if len(centers)==2:
+                            	dx= centers[0][0] - centers[1][0]
+                            	dy = centers[0][1] - centers[1][1]
+                            	D = abs(dx)
+                            	print("Distance: ")
+                            	print(D)
+                             	#TODO: Calibrate
+                             	cv2.putText(frame,str(D),(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
+            cv2.putText(frame,"Vision State: "+str(vis1),(frame.shape[1] - 400, frame.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+            cv2.putText(frame2,"Vision 2 State: "+str(vis2),(frame.shape[1] - 400, frame.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
             cv2.imshow("45c Robotics",frame)
             cv2.imshow("45c Robotics 2",frame2)
             if cv2.waitKey(1) & 0xFF == ord('q'):
