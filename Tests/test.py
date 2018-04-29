1c31f917fbde3ad9fd7c3a7fd869bc7adabc2c54
isaacaddis <isaacaddis21@gmail.com>
2018-04-29 15:42:58 -0700

    Disable vision for now

--- Tests/test.py
+++ Tests/test.py
@@ -25,24 +25,25 @@ def vis(mirror=True):
     # Default - Red
     color_mode="red"
     cap = cv2.VideoCapture(1)
-    cap.set(3,320)
-    cap.set(4,216)
+    #cap.set(3,320)
+    #cap.set(4,216)
     cap.set(5,15)
     cap2 = cv2.VideoCapture(0)
-    cap2.set(3,320)
-    cap2.set(4,216)
+   # cap2.set(3,320)
+   # cap2.set(4,216)
     cap2.set(5,15)
     cv2.namedWindow("45c Robotics", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("45c Robotics", 960,540)
+  #  cv2.resizeWindow("45c Robotics", 960,540)
     cv2.namedWindow("45c Robotics 2",cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("45c Robotics 2",960,540)
+ #   cv2.resizeWindow("45c Robotics 2",960,540)
     if mirror:
         while cap.isOpened():
             ret_val, frame = cap.read()
             frame = cv2.flip(frame, 1)
+            frame = cv2.flip(frame, 0)
             ret_val2, frame2 = cap2.read()
-            frame2 = cv2.flip(frame2, 1)
-            if keyboard.is_pressed('a'):
+           # frame2 = cv2.flip(frame2, 1)
+            if keyboard.press('a'):
                 vis1^=True
                 print("Vision state switched on Camera 0.")
             if keyboard.is_pressed('l'):
@@ -64,8 +65,8 @@ def vis(mirror=True):
                             continue
                        # if cv2.contourArea(c)>1500:
                            # continue
-                        x,y,w,h = cv2.boundingRect(c)
-                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),4)
+                       # x,y,w,h = cv2.boundingRect(c)
+                       # cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),4)
                         M = cv2.moments(c)
                         if M['m00']!=0:
                             cX = int(M['m10'] /M['m00'])
@@ -85,7 +86,7 @@ def vis(mirror=True):
                             D = D * conversion_rate
                             print(D)
                             '''
-                            cv2.putText(frame,str(D)+" pixels",(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
+                            #cv2.putText(frame,str(D)+" pixels",(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
             if vis2 == True:
                 kernel = np.ones((5,5), np.uint8)
                 img = cv2.GaussianBlur(frame2,(5,5),0)
@@ -119,8 +120,8 @@ def vis(mirror=True):
                             continue
                         if cv2.contourArea(c)>2000:
                             continue
-                        x,y,w,h = cv2.boundingRect(c)
-                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                       # x,y,w,h = cv2.boundingRect(c)
+                       # cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
                         M = cv2.moments(c)
                         if M['m00']!=0:
                             cX = int(M['m10'] /M['m00'])
@@ -138,9 +139,9 @@ def vis(mirror=True):
                             D = D * conversion_rate
                             print(D)
                             #TODO: Calibrate
-                            cv2.putText(frame,str(D),(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
-            cv2.putText(frame,"Vision State: "+str(vis1),(frame.shape[1] - 400, frame.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
-            cv2.putText(frame2,"Vision 2 State: "+str(vis2),(frame.shape[1] - 400, frame.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+                            #cv2.putText(frame,str(D),(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
+            #cv2.putText(frame,"Vision State: "+str(vis1),(frame.shape[1] - 400, frame.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+            #cv2.putText(frame2,"Vision 2 State: "+str(vis2),(frame.shape[1] - 400, frame.shape[0] - 200), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
             cv2.imshow("45c Robotics",frame)
             cv2.imshow("45c Robotics 2",frame2)
             cv2.waitKey(50)
