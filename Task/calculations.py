31ac34d8d4c791e6c9ed06d55da9759c3ff0d007
isaacaddis <isaacaddis21@gmail.com>
2018-05-11 17:05:33 -0700

    Update calcs

--- Task/calculations.py
+++ Task/calculations.py
@@ -4,9 +4,15 @@ def calculations():
     airSpeed = int(raw_input("Airspeed (ON ASCENT): "))
     ascentRate = timeUntilFailure * airSpeed
     print("Ascent vector: "+ str(ascentRate)+ " at " + str(heading) + " degrees")
+    print("On the map, this will be about: "+ str(ascentRate * .7)+ "cm")
     descentSpeed = int(raw_input("Airspeed (ON DESCENT) rate: "))
     descentRate = descentSpeed * timeUntilFailure
     print("Descent vector: "+ str(descentRate)+ " at " + str(heading) + " degrees")
+    print("On the map, this will be about: "+ str(descentRate * .7)+ "cm")
+    windPush = int(raw_input("Wind speed")) * timeUntilFailure
+    windBlowing = int(raw_input("Wind is blowing from (degrees): "))-180
+    print("Wind vector: "+ str(windPush)+ " at " + str(windBlowing) + " degrees")
+    print("On the map, this will be about: "+ str(windPush * .7)+ "cm")
 calculations()
 
 
