1daa3d0b57bbf5ebb72a3729c704a9273b8719bd
isaacaddis <isaacaddis21@gmail.com>
2018-05-20 14:22:49 -0700

    Fix Tensorflow errors

--- Robot/OBS/get_data.py
+++ Robot/OBS/get_data.py
@@ -1,10 +1,11 @@
-def gata_data(dir):
+import os
+def get_data(directory):
     for i in os.listdir(directory):
         full_path = os.path.join(directory,i)
         if os.path.isdir(full_path):
             for entry in grab_files(full_path):
                 yield entry
-            elif os.path.isfile(full_path):
-                yield full_path
-            else:
-                print('No such path %s' % full_path)
+        elif os.path.isfile(full_path):
+            yield full_path
+        else:
+            print('No such path %s' % full_path)
