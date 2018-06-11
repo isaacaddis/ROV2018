58463b545254f7d9e6802a8eb85cc5742d0b2c74
isaacaddis <isaacaddis21@gmail.com>
2018-06-11 12:28:46 -0700

    Calc focal length

--- Tests/calc.py
+++ Tests/calc.py
@@ -1,17 +1,23 @@
-fx = 1.0
-fy = 1.0
-focal_length = 1.0
-lower_res = 1024.0
-native_res = 1111.0
-object_1 = 5.0
-object_2 = 5.0
-
-
-m = ((fx+fy)/2)/focal_length
-object_img_sensor_1 = (native_res/m)*lower_res
-object_img_sensor_2 = (native_res/m)*lower_res
-
-distance_to_1 = ((object_1*focal_length)/object_img_sensor_1)**2
-distance_to_2 = ((object_1*focal_length)/object_img_sensor_2)**2
-
-distance = (distance_to_1-distance_to_2) ** .5
+import cv2
+import numpy as np
+import helpers
+KNOWN_DISTANCE = 0.3084 #.3084 meters
+KNOWN_WIDTH = 0.0135 # .0135 meters
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("Testing for Calculations", cv2.WINDOW_NORMAL)
+while cap.isOpened():
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5,5), 0)
+    edged = cv2.Canny(gray,35,125)
+    _,cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(cnts)>0:
+        c = max(cnts,key=cv2.contourArea)
+        area = cv2.minAreaRect(c)
+        x,y,w,h = cv2.boundingRect(c)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),4)
+        focalLength = helpers.calc_focal_length(KNOWN_DISTANCE,KNOWN_WIDTH,area)
+        cv2.putText(frame,str(focalLength),(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 3)
+    cv2.imshow("Testing for Calculations",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+    	break
---
+++ Tests/helpers.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+def area_of_contour(contour):
+    return cv2.minAreaRect(c)
+def distance_from_camera(knownWidth,focalLength, perWidth):
+    return (knownWidth*focalLength)/perWidth
+def calc_focal_length(knownDistance,knownWidth,areaOfContour):
+    return (areaOfContour[1][0]*knownDistance)/knownWidth
+
