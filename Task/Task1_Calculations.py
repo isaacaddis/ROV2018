3442c25b9f32fe4c65b11862d64ac8fee0b225b8
Jiajer Ho <jjh10422@gmail.com>
2018-06-20 09:44:59 -0700

    Update Task1_Calculations.py

--- Task/Task1_Calculations.py
+++ Task/Task1_Calculations.py
@@ -9,23 +9,22 @@
 
 
 def calculations():
-    heading = float(raw_input("Heading: "))
-    timeUntilFailure = float(raw_input("Time Until Failure: "))
-    ascentAirspeed = float(raw_input("Airspeed (ON ASCENT): "))
+    heading = float(raw_input("Heading (degrees): "))
+    ascentAirspeed = float(raw_input("Airspeed ascent (m/s): "))
+    timeUntilFailure = float(raw_input("Time Until Engine Failure (seconds): "))
     ascentVector = timeUntilFailure * ascentAirspeed
-    print("Ascent vector: "+ str(ascentVector)+ " at " + str(heading) + " degrees")
+    print("Ascent Distance: "+ str(ascentVector)+ " meters"+ " at " + str(heading) + " degrees")
     print("On the map, this will be about: "+ str(ascentVector * .7)+ "cm")
-    ascentRate = float(raw_input("Ascent Rate: "))
+    ascentRate = float(raw_input("Ascent Rate (m/s): "))
     height = ascentRate * timeUntilFailure
-    decentRate = float(raw_input("Decent Rate: "))
-    decentAirspeed = float(raw_input("Airspeed decent: "))
-    decentTime = height / decentRate
-    descentVector = decentTime * decentAirspeed
-    print("Descent vector: "+ str(descentVector)+ " at " + str(heading) + " degrees")
+    descentRate = float(raw_input("Descent Rate (m/s): "))
+    descentAirspeed = float(raw_input("Airspeed Descent (m/s): "))
+    descentTime = height / descentRate
+    descentVector = descentTime * descentAirspeed
+    print("Descent Distance: "+ str(descentVector)+ " meters"+ " at " + str(heading) + " degrees")
     print("On the map, this will be about: "+ str(descentVector * .7)+ "cm")
-    windPush = float(raw_input("Wind speed")) * decentTime
-    windBlowing = float(raw_input("Wind is blowing from (degrees): "))-180
-    print("Wind vector: "+ str(windPush)+ " at " + str(windBlowing) + " degrees")
+    windPush = float(raw_input("Wind Speed (m/s): ")) * descentTime
+    windBlowing = float(raw_input("Wind direction from (degrees): "))-180
+    print("Wind Distance: "+ str(windPush)+ " meters"+ " at " + str(windBlowing) + " degrees")
     print("On the map, this will be about: "+ str(windPush * .7)+ "cm")
 calculations()
-
