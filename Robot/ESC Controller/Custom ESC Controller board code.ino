f52b7ec5ab575dc1c9a8e66b727ef134e24d7a34
Jiajer Ho <jjh10422@gmail.com>
2018-07-29 22:08:57 -0700

    Create Custom ESC Controller board code.ino

---
+++ Robot/ESC Controller/Custom ESC Controller board code.ino
@@ -0,0 +1,40 @@
+/**
+ * @author Jiajer Ho and Alexander Vazquez
+ * 
+ * ROV ESC Controller Underwater Code (Receiver)  7/29/2018
+ * Use ATMEGA1260 or 2560
+ * 
+ * Takes data from Surface Arduino and determine the ESC control  
+ * 
+ */
+ 
+char mystr[4]; //Initialized variable to store recieved data
+int testOutput = 0;
+String str2 = "10";
+void setup() {
+// Begin the Serial at 9600 Baud
+Serial1.begin(9600);
+Serial.begin(9600);
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+
+digitalWrite(LED_BUILTIN, LOW); 
+Serial1.readBytes(mystr,4); //Read the serial data and store in var
+String str(mystr);
+testOutput = str.toInt();
+ if (testOutput>= 1 and testOutput<= 10) {
+    Serial.println("Forwards");  
+  
+ }
+  if (testOutput>= 11 and testOutput<= 20) {
+    Serial.println("Backwards"); 
+  
+ }
+   if (testOutput<= 1) {
+    Serial.println("NONE"); 
+  
+ }
+delay(100);
+}
