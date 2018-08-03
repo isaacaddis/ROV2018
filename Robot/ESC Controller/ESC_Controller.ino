ab3ed5fae2131aea17d33f9b336f25eb46c5eb83
Jiajer Ho <jjh10422@gmail.com>
2018-08-03 00:03:44 -0700

    Update ESC_Controller.ino

--- Robot/ESC Controller/ESC_Controller.ino
+++ Robot/ESC Controller/ESC_Controller.ino
@@ -1,17 +1,24 @@
 /**
  * @author Jiajer Ho and Alexander Vazquez
  * 
- * ROV ESC Controller Underwater Code (Receiver)  7/29/2018
+ * ROV ESC Controller Underwater Code (Receiver)  08/03/2018
  * Use ATMEGA1260 or 2560
  * 
  * Takes data from Surface Arduino and determine the ESC control  
  * 
  */
- 
+ #include <Servo.h>
+ Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+
 char mystr[4]; //Initialized variable to store recieved data
 int testOutput = 0;
 String str2 = "10";
 void setup() {
+  myservo.attach(9); 
 // Begin the Serial at 9600 Baud
 Serial1.begin(9600);
 Serial.begin(9600);
@@ -21,20 +28,22 @@ pinMode(LED_BUILTIN, OUTPUT);
 void loop() {
 
 digitalWrite(LED_BUILTIN, LOW); 
-Serial.readBytes(mystr,4); //Read the serial data and store in var
+Serial1.readBytes(mystr,4); //Read the serial data and store in var
 String str(mystr);
 testOutput = str.toInt();
  if (testOutput>= 1 and testOutput<= 10) {
-    Serial1.println("Forwards");  
-  
+    Serial.println("Forwards");  
+    pos = map(testOutput, 1, 10, 90, 165);
+  myservo.write(pos); 
  }
   if (testOutput>= 11 and testOutput<= 20) {
-    Serial1.println("Backwards"); 
-  
+    Serial.println("Backwards"); 
+      pos = map(testOutput, 11, 20, 90, 24);
+  myservo.write(pos); 
  }
    if (testOutput<= 1) {
-    Serial1.println("NONE"); 
-  
+    Serial.println("NONE"); 
+  myservo.write(90); 
  }
 delay(100);
 }
