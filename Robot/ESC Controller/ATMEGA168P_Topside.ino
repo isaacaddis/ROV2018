998b0fed3ccf40dc31f15d1360e5b4c2eb8d0123
Jiajer Ho <jjh10422@gmail.com>
2018-08-08 20:07:56 -0700

    Update ATMEGA168P_Topside.ino

--- Robot/ESC Controller/ATMEGA168P_Topside.ino
+++ Robot/ESC Controller/ATMEGA168P_Topside.ino
@@ -8,32 +8,53 @@
  * 
  */
  
-const int analogInPin = A0;
-int sensorValue = 0;        // value read from the pot
+const int InputA0 = A0;
+int PotA0 = 0;        // value read from the pot
+int SpeedA0 = 0;
+const int InputA1 = A1;
+int PotA1 = 0;        // value read from the pot
+int SpeedA1 = 0;
 int outputValue = 0;
-int outputValue2 = 0;
-int n =0;
+int n = 0;
 String str;
 String str2;
 char mystr[5];
 void setup() {
   // Begin the Serial at 9600 Baud
- delay(1000);
+ delay(500);
   Serial.begin(9600);
 }
 
 void loop() {
-  sensorValue = analogRead(analogInPin);
-  outputValue2 = map(sensorValue, 0, 1023, 0, 100);
-  if(outputValue2 >= 55 and outputValue2 <= 100){
-    outputValue = map(outputValue2, 55, 100, 0, 10);
+  PotA0 = analogRead(InputA0);
+  PotA1 = analogRead(InputA1);
+  SpeedA0 = map(PotA0, 0, 1023, 0, 100);
+  SpeedA1 = map(PotA1, 0, 1023, 0, 100);
+  if(SpeedA0 >= 55 and SpeedA0 <= 100)
+  {
+    outputValue = map(SpeedA0, 55, 100, 0, 10);
   }
-    if(outputValue2 >= 0 and outputValue2 <= 45){
-    outputValue = map(outputValue2, 0, 45, 20, 11);
+  else if(SpeedA0 >= 0 and SpeedA0 <= 45)
+  {
+    outputValue = map(SpeedA0, 0, 45, 20, 11);
   }
-      if(outputValue2 >= 45 and outputValue2 <= 55){
-    outputValue = 0;
-  }
+  else
+  {
+    if(SpeedA1 >= 55 and SpeedA1 <= 100)
+    {
+    outputValue = map(SpeedA1, 55, 100, 0, 10);
+    }
+    else if(SpeedA1 >= 0 and SpeedA1 <= 45)
+    {
+    outputValue = map(SpeedA1, 0, 45, 20, 11);
+    }
+    else
+    {
+     outputValue = 0;
+    }
+   }
+
+  
   str=String(outputValue);
 
   str.toCharArray(mystr, 4);
