1f50ff9e94710acd00d0f62bf37cc364bbd45ddc
PhysicsAlex <35387024+PhysicsAlex@users.noreply.github.com>
2018-09-16 16:57:55 -0700

    Pivot Works Now
    Marcos Helped.

--- Robot/ESC Controller/ATMEGA168P_Topside.ino
+++ Robot/ESC Controller/ATMEGA168P_Topside.ino
@@ -1,16 +1,40 @@
 #include "Servo.h"
 
 const int InputA0 = A0;
+const int InputA1 = A1;
+
 int PotA0 = 0;        // value read from the pot
 int SpeedA0 = 0;
-const int InputA1 = A1;
 int PotA1 = 0;        // value read from the pot
 int SpeedA1 = 0;
+
 int outputValueX = 0;
 int outputValueY = 0;
+
 int n = 0;
+
+int XDirection = 0;
+int YDirection = 0;
+
+int SpeedY = 0;
+int SpeedX = 0;
+
+char sendsig[20];
+
 String str1;
 String str2;
+String str3;
+
+String strSpeedY;
+String strSpeedX;
+
+String strDirectX;
+String strDirectY;
+
+String ultimateStr;
+
+
+const int unitOne = 1;
 
 Servo f_Left;
 Servo f_Right;
@@ -28,8 +52,8 @@ void goForward(int param) {
   b_Left.write(velocity * -1);
   b_Right.write(velocity * -1);
 
-  Serial.println("Current Command: Move Forward");
-  Serial.println(" ");
+//  Serial.println("Current Command: Move Forward");
+
 }
 
 void goBackward(int param) {
@@ -43,8 +67,8 @@ void goBackward(int param) {
   b_Left.write(velocity);
   b_Right.write(velocity);
 
-  Serial.println("Current Command: Move Backward");
-  Serial.println(" ");
+//  Serial.println("Current Command: Move Backward");
+  
 }
 
 void goLeft(int param) {
@@ -58,9 +82,8 @@ void goLeft(int param) {
   b_Left.write(velocity * -1);
   b_Right.write(velocity);
 
-  
-  Serial.println("Current Command: Move Left");
-  Serial.println(" ");
+//  Serial.println("Current Command: Move Left");
+
 }
 void goRight(int param) {
   int percent = abs(param/10);
@@ -73,8 +96,18 @@ void goRight(int param) {
   b_Left.write(velocity);
   b_Right.write(velocity * -1);
 
-  Serial.println("Current Command: Move Right");
-  Serial.println(" ");
+//  Serial.println("Current Command: Move Right");
+
+}
+
+void noMove() {
+  int velocity = 0;
+  f_Left.write(velocity);
+  f_Right.write(velocity);
+  b_Left.write(velocity);
+  b_Right.write(velocity );
+
+//  Serial.println("Current Command: Don't Move");
 }
 
 void setup() {
@@ -90,62 +123,118 @@ b_Right.attach(11);
 void loop() {
   PotA0 = analogRead(InputA0);
   PotA1 = analogRead(InputA1);
-  SpeedA0 = map(PotA0, 0, 1023, 0, 100);
-  SpeedA1 = map(PotA1, 0, 1023, 0, 100);
-  if(SpeedA0 >= 55 and SpeedA0 <= 100)
-  {
-    //backwards
-    outputValueY = map(SpeedA0, 55, 100, 0, -10);
+  
+  outputValueY = map(PotA0, 0, 1023, -10, 10);
+  outputValueX = map(PotA1, 0, 1023, -10, 10);
+  
+  int x = outputValueX;
+  int y = outputValueY; 
+
+  if( ((x <= -2 and x >= -9) and abs(x) != unitOne) and ((abs(y) >= x ) and y != 10)){
+     goRight(outputValueX);
+     XDirection = -1;
+     YDirection = 0;
+}
+  else if((x == -10) and (abs(y) <= 9)){
+    goRight(outputValueX);
+    XDirection = -1;
+    YDirection = 0;
+}
+       
+  if( (x >= 2 and x <= 9) and (abs(y) <= x)){
+    goLeft(outputValueX);
+    XDirection = 1;
+    YDirection = 0;
   }
-  else if(SpeedA0 >= 0 and SpeedA0 <= 45)
-  {
-    //forwards
-    outputValueY = map(SpeedA0, 0, 45, 0, 10);
+  else if((x == 10) and ( abs(y) <= 9)){
+    goLeft(outputValueX);
+    XDirection = 1;
+    YDirection = 0;
   }
-  else
-  {
-    if(SpeedA1 >= 55 and SpeedA1 <= 100)
-    {
-    outputValueX = map(SpeedA1, 55, 100, 0, 10);
-    //right
-    }
-    else if(SpeedA1 >= 0 and SpeedA1 <= 45)
-    {
-    outputValueX = map(SpeedA1, 0, 45, 0, -10);
-    }
-    //left
-    else
-    {
-     outputValueX = 0;
-     outputValueY = 0;
-     
-    }
-   }  
+
+ 
+  if( ((x >= -9 and x <= 9) and y != 1) and (y > abs(x))){
+     goForward(outputValueY);
+     YDirection = 1;
+     XDirection = 0;
+  }
+  if( ( abs(x) == 10 ) and (y == 10 )){
+     goForward(outputValueY);
+     YDirection = 1;
+     XDirection = 0;
+  }
+  else if((x == 0) and (y >= 2 and abs(y) != unitOne) ){
+    goForward(outputValueY);
+    YDirection = 1;
+    XDirection = 0;
+  }
+  
    
-   if(outputValueY < 0){ 
-    goBackward(outputValueY); 
-   }
-  else if(outputValueY > 0){ 
-    goForward(outputValueY); 
-   }
-   if(outputValueX < 0){ 
-    goLeft(outputValueX); 
-   }
-  else if(outputValueX > 0){ 
-    goRight(outputValueX); 
-   }
+  if( ((x >= -9 and x <= 9 ) and x != 1) and (abs(y) < abs(x) * -1 )){
+     goBackward(outputValueY);
+     YDirection = -1;
+     XDirection = 0;
+  }
+  if( ( abs(x) == 10 ) and (y == -10 )){
+     goBackward(outputValueY);
+     YDirection = -1;
+     XDirection = 0;
+  }
+  else if((x == 0) and (y <= -2 and abs(y) != unitOne) ){
+    goBackward(outputValueY);
+    YDirection = -1;
+    XDirection = 0;
+  }    
+
+  if( (x < 2 and x > -2) and (y < 2 and y > -2) ){
+    noMove();
+    YDirection = 0;
+    XDirection = 0; 
+  }
+
+  SpeedY = YDirection * outputValueY;
+  SpeedX = XDirection * outputValueX;
+
+  str1 = String(outputValueX);
+  str2 = String(outputValueY); 
+  str3 = String(str1+ " , " + str2);
+
+  strDirectY = String(YDirection);
+  strDirectX = String(XDirection);
   
-  str1=String(outputValueX);
-  str2=String(outputValueY); 
+  strSpeedY = String(SpeedY);
+  strSpeedX = String(SpeedX);
 
-
-
-  //str.toCharArray(mystr, 4);
-  Serial.println("X Position: ");
-  Serial.println(str1); //Write the serial data
-  Serial.println("Y Position: ");
-  Serial.println(str2); //Write the serial data
-  Serial.println(" ");
+  ultimateStr = String(strDirectX + " ," + strDirectY + " ," + strSpeedX + " ," + strSpeedY + "\n");
   
+//  Serial.println("X and Y Coordinates: ");
+//  Serial.println(str3);
+//  
+//  Serial.println("Speed along the Y axis: ");
+//  Serial.println(strSpeedY);
+//    
+//  Serial.println("Speed along the X axis: ");
+//  Serial.println(strSpeedX);
+//
+//  Serial.println("Direction along the Y axis: ");
+//  Serial.println(strDirectY);
+//  
+//  Serial.println("Direction along the X axis: ");
+//  Serial.println(strDirectX);
+//  
+ 
+//  strDirectY.toCharArray(sendsig, 3);
+//  strDirectX.toCharArray(sendsig, 3);
+//  strSpeedX.toCharArray(sendsig, 3);
+//  strSpeedY.toCharArray(sendsig, 3);
+
+  ultimateStr.toCharArray(sendsig, 20);
+  Serial.write(sendsig, 20); //Write the serial data
+ 
+
   delay(100);
+
+
+  
+
 }
